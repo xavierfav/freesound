93f8dee95d6ed2c5dd490799f4b2655a726ff4d1
xavierfav <xavier.favory@gmail.com>
2020-01-14 15:01:30 +0100

    Add play cluster sound examples from facets

--- search/views.py
+++ search/views.py
@@ -173,7 +173,7 @@ def clustering_facet(request):
     if result['finished']:
         if result['result'] is not None:
             results = result['result']
-            num_clusters = len(results) + 1
+            num_clusters = len(results)
         else:
              return JsonResponse({'status': 'failed'}, safe=False)
     elif result['error']:
@@ -187,6 +187,7 @@ def clustering_facet(request):
     # label clusters using most occuring tags
     sound_instances = sounds.models.Sound.objects.bulk_query_id(map(int, classes.keys()))
     sound_tags = {sound.id: sound.tag_array for sound in sound_instances}
+    sound_urls = {sound.id: sound.locations()['preview']['LQ']['ogg']['url'] for sound in sound_instances}
     cluster_tags = defaultdict(list)
 
     # extract tags for each clusters and do not use query terms for labeling clusters
@@ -199,10 +200,18 @@ def clustering_facet(request):
     cluster_most_occuring_tags = [' '.join(zip(*Counter(cluster_tags[cluster_id]).most_common(3))[0]) 
                                   for cluster_id in range(len(results))]
 
+    # extract sound examples for each cluster
+    graph = result['graph']
+    node_id_centrality_per_cluster = defaultdict(list)
+    for node in graph['nodes']:
+        node_id_centrality_per_cluster[node['group']].append((node['id'], node['group_centrality'], sound_urls[int(node['id'])]))
+    sorted_node_id_centrality_per_cluster = {k: sorted(v, key=lambda x: x[1], reverse=True) for k, v in node_id_centrality_per_cluster.iteritems()}
+    sound_url_examples_per_cluster = [[s[2] for s in sorted_node_id_centrality_per_cluster[k][:7]] for k in range(num_clusters)]
+
     return render(request, 'search/clustering_facet.html', {
             'results': classes,
             'url_query_params_string': url_query_params_string,
-            'cluster_id_num_results': zip(range(num_clusters), num_sounds_per_cluster, cluster_most_occuring_tags),
+            'cluster_id_num_results': zip(range(num_clusters), num_sounds_per_cluster, cluster_most_occuring_tags, sound_url_examples_per_cluster),
     })
 
 
